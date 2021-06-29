976b27fcf1c7a87c78a65e441889323bfc42a347
Victor Nogueira <ovictorhnogueira@gmail.com>
2021-06-29 00:39:07 -0300

    fix: added express.urlencoded and express.json middlewares

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import routes from './routes'
 
 const app = express()
+app.use(express.urlencoded({ extended: true })) 
+app.use(express.json())
 app.use(routes)
 
 export default app
